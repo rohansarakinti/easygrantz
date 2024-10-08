c40bbfaa9577e67ac1c8053dcf3c9847bd7dfd41
rohansarakinti <sarakintirohan@gmail.com>
2024-10-07 21:11:08 -0500

    minor metadata fix

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Page from './homepage.jsx'
 
 export const metadata = {
     title: "Home",
-    description: "EasyGrantz helps students discover over 175+ grants and microloans to fund their business or nonprofit ventures. Explore our hand-curated database with simple filters and search options to find the perfect funding opportunity. We're here to turn problems into solutions! Partnered with Chick-fil-A.",
+    description: "EasyGrantz helps students discover over 200+ grants and microloans to fund their business or nonprofit ventures. Explore our hand-curated database with simple filters and search options to find the perfect funding opportunity. We're here to turn problems into solutions! Partnered with Chick-fil-A.",
   }
 
 function page() {
